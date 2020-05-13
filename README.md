16b143f21939868efe43279d6aeeb840711235aa
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-05-13 12:10:56 +0200

    Initial Version

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Purgecss with Gutenberg
+
+purgecss whitelist for the WordPress Gutenberg editor.
+
+## Getting started
+
+**Installation**
+First install [purgecss](https://github.com/FullHuman/purgecss).
+
+`npm i --save-dev purgecss-with-gutenberg`
+
+## Usage
+```javascript
+import Purgecss from 'purgecss';
+import purgecssGutenberg from 'purgecss-with-gutenberg';
+
+const purgeCss = new Purgecss({
+	whitelist: purgecssGutenberg.whitelist,
+	whitelistPatterns: purgecssGutenberg.whitelistPatterns
+});
+
+const res = purgeCss.purge();
+```
+
+if you have additional classes for `whitelist` or `whitelistPattners`, you can use the spread operator:
+
+```javascript
+whitelist: [
+	...purgecssGutenberg.whitelist,
+	...thirdparty.whitelist,
+	'my-class'
+],
+whitelistPatterns: [
+	...purgecssGutenberg.whitelistPatterns,
+	...thirdparty.whitelistPatterns,
+	/^my-(class|id)$/
+]
+```
---
+++ index.js
@@ -0,0 +1,60 @@
+module.exports = {
+	whitelist: [
+		// Utility Classes
+		'has-background',
+		'has-background-gradient',
+		'has-drop-cap', 
+		'wp-has-aspect-ratio',
+		'is-resized',
+		'is-cropped',
+		'has-parallax',
+		'is-grid',
+		'has-dates',
+		'is-stacked-on-mobile',
+		'is-image-fill',
+		'has-avatars',
+
+		// core/cover
+		'wp-block-cover__gradient-background',
+		'wp-block-cover__inner-container',
+
+		// core/gallery
+		'blocks-gallery-grid',
+		'blocks-gallery-item',
+
+		// core/calendar
+		'wp-calendar',
+		'wp-calendar-table',
+		'wp-calendar-nav',
+
+	],
+	whitelistPatterns: [
+		// Default Block Classes
+		/^wp-block(-.*)?$/,
+
+		// Utility Classes
+		/^is-style(-.*)?$/,
+		/^has-(.*)?-color$/,
+		/^has-(.*)?-background$/,
+		/^has-text-align-(left|right|center|justify)$/,
+		/^align(left|right|center|wide|full)$/,
+		/^has-(.*)?-font-size$/,
+		/^(is|are)-vertically-aligned-(top|center|bottom)$/,
+
+		// core/cover
+		/^has-background-dim-\d+$/,
+
+		// core/gallery
+		/^columns-(\d+)$/,
+
+		// core/media-text
+		/^has-media-on-the-(left|right)$/,
+
+		// core/embed
+		/^wp-embed-aspect-(\d+-\d+)?$/,
+
+		// core/social-links
+		/^wp-social-link(-.*)?$/
+
+	]
+};
